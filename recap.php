60dbfdeafc12bf3715493bff89b0ac1699a190d1
seitvalieva <sseitvalieva@gmail.com>
2024-05-21 23:55:47 +0200

    calcul et affichage du total

--- recap.php
+++ recap.php
@@ -9,6 +9,41 @@
     <title>Recap of the product</title>
 </head>
 <body>
-    <?php var_dump($_SESSION); ?>
+    <!-- <?php var_dump($_SESSION); ?> -->
+    <?php 
+    if(!isset($_SESSION['products']) || empty($_SESSION['products'])){
+        echo "<p>Aucun produit en session...</p>";
+    }
+    else{
+        echo "<table>",
+                "<thead>",
+                    "<tr>",
+                        "<th>#</th>",
+                        "<th>Nom</th>",
+                        "<th>Prix</th>",
+                        "<th>Quantite</th>",
+                        "<th>Total</th>",
+                    "</tr>",
+                "</thead>",
+                "<tbody>";
+    $totalGeneral = 0;
+    foreach($_SESSION['products'] as $index => $product){
+        echo "<tr>",
+            "<td>".$index."</td>",
+            "<td>".$product['name']."</td>",
+            "<td>".number_format($product['price'], 2, ",", "&nbsp;")."&nbsp;€</td>",
+            "<td>".$product['qqt']."</td>",
+            "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€</td>",
+            "</tr>";
+        $totalGeneral +=$product['total'];
+        }
+    echo "<tr>",
+        "<td colspan=4>Total general :</td>",
+        "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
+        "</tr>",
+        "</tbody>",
+        "</table>";
+    }
+    ?>
 </body>
 </html>
